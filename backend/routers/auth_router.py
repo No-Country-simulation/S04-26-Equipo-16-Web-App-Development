5001c8a9c7906c349ac0d1ec6ee517722892a854
matiasDev4 <matiasalvarezoaks@gmail.com>
2026-04-30 20:20:43 -0300

    feature: encargado de los endpoists para registrar, loguear al usuario y autorización por rol

--- backend/routers/auth_router.py
+++ backend/routers/auth_router.py
@@ -1,9 +1,10 @@
 from fastapi.routing import APIRouter
+from backend.config.connection import DB_DEPENDS
 
 auth_router = APIRouter(tags=['Rutas de autenticación'])
 
 @auth_router.post('/api/auth/register')
-def register_user():
+def register_user(db = DB_DEPENDS):
     pass
 
 @auth_router.post('/api/auth/login')
